79fb83e6b8da354e45fa8f4185ac708133320ac8
gaoqi <1224362143@qq.com>
2023-06-02 16:02:37 +0800

    chore: 修改ws返回方法

--- src/webscoket/ws.adapter.ts
+++ src/webscoket/ws.adapter.ts
@@ -43,22 +43,6 @@ export class WsAdapter implements WebSocketAdapter {
     let message = null;
     try {
       message = JSON.parse(buffer.data);
-      const { whisper } = message;
-      if (whisper === '你真帅') {
-        const data = {
-          code: 0,
-          msg: '这是实话，继续，我爱听',
-        };
-        client.send(JSON.stringify(data));
-        return EMPTY;
-      } else if (whisper) {
-        const data = {
-          code: 100001,
-          msg: '抱歉，我没听懂，你可以说一点我听得懂的，比如 你真帅',
-        };
-        client.send(JSON.stringify(data));
-        return EMPTY;
-      }
     } catch (error) {
       console.log('ws解析json出错', error);
       return EMPTY;
--- src/webscoket/ws.gateway.ts
+++ src/webscoket/ws.gateway.ts
@@ -1,4 +1,6 @@
+import * as WebSocket from 'ws';
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
@@ -11,7 +13,33 @@ export class WebStartGatWay {
     return {
       event: 'hello',
       data: data,
-      msg: 'success',
+      msg: 'hello',
+    };
+  }
+  @SubscribeMessage('你真帅')
+  handsome(
+    @MessageBody() data: any,
+    @ConnectedSocket() client: WebSocket,
+  ): any {
+    console.log('收到消息 client:', client);
+    client.send(
+      JSON.stringify({
+        event: 'tmp',
+        data: '这里是个临时信息',
+        msg: '你是个诚实的人',
+      }),
+    );
+    return { event: 'hello2', data: data, msg: '希望你可以更诚实一点' };
+  }
+  @SubscribeMessage('你最帅了')
+  TheMostHandsome(
+    @MessageBody() data: any,
+    @ConnectedSocket() client: WebSocket,
+  ): any {
+    return {
+      event: 'hello2',
+      data: data,
+      msg: '是的、这是毫无疑问的、你有资格获得我的爱',
     };
   }
 }
