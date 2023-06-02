2c9761ad86394e70778575b109a531ddd2c5679a
gaoqi <1224362143@qq.com>
2023-06-02 17:11:50 +0800

    chore: 修改转发地址

--- src/uc/uc.service.ts
+++ src/uc/uc.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs';
 export class UcService {
   constructor(private Http: HttpService) {}
   getLocate() {
-    return this.Http.get('https://api-test.papegames.com:12101/v1/ip/locate', {
+    return this.Http.get('https://url', {
       params: {
         clientid: 1083,
         timestamp: 1685675259,
@@ -17,7 +17,7 @@ export class UcService {
 
   getprofile() {
     return this.Http.post(
-      'https://api-test.papegames.com:12101/v1/user/getprofile?timestamp=1685675259&sig=4ef37148e63d1e860bdcc089202c6383&clientid=1083&lang=zh',
+      'https://url',
       {
         nid: 2498870,
         token: '7f0ddbc7b9300965798dea495123828f1ded1895',
